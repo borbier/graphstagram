721655e19f06a9cb9f9867022b28b8caf0249407
_borBeer <60010842@kmitl.ac.th>
2019-06-01 22:03:47 +0700

    :lipstick: Add image button

--- src/page/Intro.js
+++ src/page/Intro.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useState } from 'react'
 import styled from 'styled-components'
 import logo from '../assets/image/logo.png'
-import { Button, Radio, Icon } from 'antd'
+import architectLogo from '../assets/image/icon-architect.png'
+import architectLogoHovered from '../assets/image/icon-architect-hover.png'
+import customerLogo from '../assets/image/icon-customer.png'
+import customerLogoHovered from '../assets/image/icon-customer-hover.png'
+import { Button, Input, Divider, Row, Col } from 'antd'
 import { withRouter } from 'react-router-dom'
 
 const Whole = styled.div`
@@ -24,21 +28,43 @@ const ButtonHatch = withRouter(({ history }) => (
   <Button
     type="primary"
     shape="round"
-    icon="search"
     size="large"
     onClick={() => {
       history.push('/polls')
     }}
   >
-    Search
+    Login
   </Button>
 ))
 
+function ImageButton(props) {
+  const [isHovered, setHovered] = useState(false)
+
+  return(
+    <div>
+      <img 
+        src={isHovered ? props.imageHovered: props.image} 
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        width="100" 
+        height="100" />
+    </div>
+  )
+}
+
 const Intro = () => (
   <Whole>
-    <Image src={logo} alt="" />
+    <Divider><h4>GRAPH-Stagram</h4></Divider>
+    <Input placeholder="Username" />
+    <Input placeholder="Password" />
+    <Divider><h4>Type</h4></Divider>
+    <Row>
+      <Col>
+        <ImageButton image={architectLogo} imageHovered={architectLogoHovered} />
+        <ImageButton image={customerLogo} imageHovered={customerLogoHovered} />
+      </Col>
+    </Row>
     <Space />
-    <h3>GRAPH-Stagram</h3>
     <ButtonHatch />
   </Whole>
 )
