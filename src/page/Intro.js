3202ff69c7e6f11d6dcc8418498b4452c94f99b8
Chinnatip Taemkaeo <chinnatip@me.com>
2019-06-02 02:26:28 +0700

    styled#4

--- src/page/Intro.js
+++ src/page/Intro.js
@@ -51,20 +51,27 @@ const Flexer = styled.div`
   ._spanner {
     width: 54px;
   }
+  img {
+    cursor: pointer;
+  }
 `
 
-const ButtonHatch = withRouter(({ history }) => (
-  <Button
-    type="primary"
-    shape="round"
-    size="large"
-    onClick={() => {
-      history.push('/polls')
-    }}
-  >
-    Ready to Design
-  </Button>
-))
+const ButtonHatch = withRouter(({ history, current }) => {
+  const text = current === 'designer' ? 'Ready to Organize' : 'Ready to Design'
+  const link = current === 'designer' ? '/dashboard' : '/polls'
+  return (
+    <Button
+      type="primary"
+      shape="round"
+      size="large"
+      onClick={() => {
+        history.push(link)
+      }}
+    >
+      {text}
+    </Button>
+  )
+})
 
 function ImageButton(props) {
   const [isHovered, setHovered] = useState(false)
@@ -81,62 +88,100 @@ function ImageButton(props) {
   )
 }
 
-const Intro = () => (
-  <Container>
-    <img
-      src={background}
-      style={{
-        position: 'absolute',
-        right: 0,
-        top: '-40px',
-        height: '273px',
-        opacity: '0.65'
-      }}
-      alt=""
-    />
-    <img
-      src={background}
-      style={{
-        position: 'absolute',
-        left: 0,
-        bottom: '-40px',
-        height: '273px',
-        opacity: '0.4',
-        transform: 'scale(-1)'
-      }}
-      alt=""
-    />
-    <div className="_container">
-      <FountIcon>{`Graph<>Stagram`}</FountIcon>
-      <Divider>Login or register</Divider>
-      <Input
-        size="large"
-        addonBefore="Username"
-        placeholder="Username"
-        style={{ maxWidth: '360px' }}
-      />
-      <div style={{ height: '12px' }} />
-      <Input
-        size="large"
-        addonBefore="Password"
-        placeholder="Password"
-        style={{ maxWidth: '360px' }}
-      />
+class Intro extends React.Component {
+  state = {
+    currentRole: 'client'
+  }
 
-      <Divider>Select your role</Divider>
-      <Flexer>
-        <ImageButton
-          image={architectLogo}
-          imageHovered={architectLogoHovered}
+  constructor() {
+    super()
+    this.setCurrent = this.setCurrent.bind(this)
+  }
+
+  setCurrent(current) {
+    this.setState({
+      currentRole: current
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        <img
+          src={background}
+          style={{
+            position: 'absolute',
+            right: 0,
+            top: '-40px',
+            height: '273px',
+            opacity: '0.65'
+          }}
+          alt=""
         />
-        <span className="_spanner" />
-        <ImageButton image={customerLogo} imageHovered={customerLogoHovered} />
-      </Flexer>
-      <Divider />
-      <div>
-        <ButtonHatch />
-      </div>
-    </div>
-  </Container>
-)
+        <img
+          src={background}
+          style={{
+            position: 'absolute',
+            left: 0,
+            bottom: '-40px',
+            height: '273px',
+            opacity: '0.4',
+            transform: 'scale(-1)'
+          }}
+          alt=""
+        />
+        <div className="_container">
+          <FountIcon>{`Graph<>Stagram`}</FountIcon>
+          <Divider>Login or register</Divider>
+          <Input
+            size="large"
+            addonBefore="Username"
+            placeholder="Username"
+            style={{ maxWidth: '360px' }}
+          />
+          <div style={{ height: '12px' }} />
+          <Input
+            size="large"
+            addonBefore="Password"
+            placeholder="Password"
+            style={{ maxWidth: '360px' }}
+          />
+          <Divider>Select your role</Divider>
+          <Flexer>
+            <div>
+              <img
+                src={
+                  this.state.currentRole === 'designer'
+                    ? architectLogoHovered
+                    : architectLogo
+                }
+                onClick={() => this.setCurrent('designer')}
+                width="100"
+                height="100"
+              />
+            </div>
+            <span className="_spanner" />
+            <div>
+              <img
+                src={
+                  this.state.currentRole === 'client'
+                    ? customerLogoHovered
+                    : customerLogo
+                }
+                onClick={() => this.setCurrent('client')}
+                width="100"
+                height="100"
+              />
+            </div>
+          </Flexer>
+          <Divider />
+          <div>
+            <ButtonHatch current={this.state.currentRole} />
+          </div>
+        </div>
+      </Container>
+    )
+  }
+}
+
 export default Intro
